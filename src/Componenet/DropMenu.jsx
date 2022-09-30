16ff83682e11ea20f5449bff70eb137bd40aacec
rutvij-fsd <rutvijpatel414@gmail.com>
2022-09-30 21:45:38 +0530

    Setup Ready

---
+++ src/Componenet/DropMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DropMenu = () => {
+  return (
+    <div className='w-full text-white'>
+      <h1>REACT DROP DOWN MENU</h1>
+    </div>
+  )
+}
+
+export default DropMenu
