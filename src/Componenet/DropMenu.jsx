0d39b91daaf9f3c0d861543740093ba518cf9a96
rutvij-fsd <rutvijpatel414@gmail.com>
2022-09-30 22:37:15 +0530

    Drop Down menu added

--- src/Componenet/DropMenu.jsx
+++ src/Componenet/DropMenu.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import {React,Fragment } from 'react'
+import { Menu , Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const DropMenu = () => {
   return (
-    <div className='w-full text-white'>
-      <h1>REACT DROP DOWN MENU</h1>
+    <div className='w-full text-white flex flex-col items-center p-8'>
+      <h1 className='text-3xl p-8'>REACT DROP DOWN MENU</h1>
+      <h1 className='text-2xl p-8'>Should you use a dropdown?</h1>
+      {/* Drop Down Menu using Headlessui */}
+      <Menu as="div" className="relative inline-block text-left">
+        <div className='w-56'>
+          {/* menu button */}
+          <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+            Select
+            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Yes
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Probably Not
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </div>
   )
 }
